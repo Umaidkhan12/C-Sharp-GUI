57397e957eb75d27e01a73edcbbe9a50c396f4ff
Ajayxoxo <123183656+Ajayxoxo@users.noreply.github.com>
2024-08-24 19:18:17 +0530

    Add project files.

---
+++ GUI/AddRotator.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddRotator.aspx.cs" Inherits="GUI.AddRotator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:AdRotator ID="AdRotator1" runat="server" Height="200" Width="200" AdvertisementFile="~/AddRotatorController.xml" Target="_blank"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ GUI/Arithmatic.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUI
+{
+    public partial class Arithmatic : System.Web.UI.Page
+    {
+        Label input1Lb, input2Lb, resultLb;
+        TextBox input1Tb, input2Tb, resultTb;
+        Button add,sub,mul,div;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            input1Lb = new Label() { Text = "Number 1" };
+            input2Lb = new Label() { Text = "Number 2" };
+            resultLb = new Label() { Text = "Result" };
+
+            input1Tb = new TextBox();
+            input2Tb = new TextBox();
+            resultTb = new TextBox() { ReadOnly = true};
+
+            add = new Button() { Text = "Additon" };
+            sub = new Button() { Text = "Subraction" };
+            mul = new Button() { Text = "Multiplication" };
+            div = new Button() { Text = "Divition" };
+
+            add.Click += new EventHandler(AddNumber);
+            sub.Click += new EventHandler(SubNumber);
+            mul.Click += new EventHandler(MulNumber);
+            div.Click += new EventHandler(DivNumber);
+
+            form1.Controls.Add(input1Lb);
+            form1.Controls.Add(input1Tb);
+            form1.Controls.Add(new LiteralControl("<br />"));
+            form1.Controls.Add(input2Lb);
+            form1.Controls.Add(input2Tb);
+            form1.Controls.Add(new LiteralControl("<br />"));
+            form1.Controls.Add(resultLb);
+            form1.Controls.Add(resultTb);
+            form1.Controls.Add(new LiteralControl("<br /><br />"));
+            form1.Controls.Add(add);
+            form1.Controls.Add(sub);
+            form1.Controls.Add(mul);
+            form1.Controls.Add(div);
+        }
+
+        private void DivNumber(object sender, EventArgs e)
+        {
+            resultTb.Text = (Convert.ToDecimal(input1Tb.Text) / Convert.ToDecimal(input2Tb.Text)).ToString();
+        }
+
+        private void MulNumber(object sender, EventArgs e)
+        {
+            resultTb.Text = (Convert.ToDecimal(input1Tb.Text) * Convert.ToDecimal(input2Tb.Text)).ToString();
+        }
+
+        private void SubNumber(object sender, EventArgs e)
+        {
+            resultTb.Text = (Convert.ToDecimal(input1Tb.Text) - Convert.ToDecimal(input2Tb.Text)).ToString();
+        }
+
+        private void AddNumber(object sender, EventArgs e)
+        {
+            resultTb.Text = (Convert.ToDecimal(input1Tb.Text) + Convert.ToDecimal(input2Tb.Text)).ToString();
+        }
+    }
+}
